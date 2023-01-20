d5729a745594c798713180e9568825d53bc413fc
um9998 <119823374+um9998@users.noreply.github.com>
2023-01-20 13:13:21 +0900

    Add files via upload

---
+++ dmuseum/loading.js
@@ -0,0 +1,16 @@
+$(function () {
+  $("#c1")
+    .find("img")
+    .click(function () {
+      $(this).css("display", "none");
+    });
+  $("#c2").click(function () {
+    $(this).css("display", "none");
+  });
+  $("#c3").click(function () {
+    $(this).css("display", "none");
+  });
+  $("#c4").click(function () {
+    $(this).css("display", "none");
+  });
+});
